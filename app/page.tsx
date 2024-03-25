88576552415d337475e217f82394e04a3ebf9f13
varbees <harshavar968@gmail.com>
2024-03-25 14:43:53 +0530

    shadcn next14 starter kit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen bg-white flex place-items-center justify-center">
+      <Button size="lg" variant="destructive" >Auth Robust</Button>
+    </div>
+  );
+}
